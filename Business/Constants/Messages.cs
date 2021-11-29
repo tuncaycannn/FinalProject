09626b0e7466076549d4993f3f5b7867d6e87085
Tuncay <72544552+tuncaycannn@users.noreply.github.com>
2021-11-29 13:39:23 +0300

    .

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInValid = "Ürün ismi geçersiz.";
+        public static string MaintenanceTime  = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
